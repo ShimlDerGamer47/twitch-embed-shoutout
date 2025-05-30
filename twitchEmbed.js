2727f915473e2c12fc41ce127a52d01df6314131
Shiml_der_Gamer47 <161841886+ShimlDerGamer47@users.noreply.github.com>
2025-05-30 20:58:19 +0200

    twitchEmbed.js

--- twitchEmbed.js
+++ twitchEmbed.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
       return;
     }
 
-    const domain = window.location.hostname;
+    const domain = window.location.href;
 
     twitchEmbed.src = `https://clips.twitch.tv/embed?clip=${clipId}&parent=${domain}&autoplay=true&muted=false`;
   }
