6a5e0f75816cffaf3ad76f339f3c3e3667e12a7d
Shiml_der_Gamer47 <161841886+ShimlDerGamer47@users.noreply.github.com>
2025-05-30 21:17:08 +0200

    twitchEmbed.js

--- twitchEmbed.js
+++ twitchEmbed.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Twitch Clip Embed URL setzen
-  function twitchEmbedToken() {
-    const twitchEmbed = document.getElementById("twitchEmbedId");
-    if (!twitchEmbed) return;
+  function twitchEmbedContainerToken() {
+    const twitchEmbedContainer = document.getElementById(
+      "twitchEmbedContainerId"
+    );
+    if (!twitchEmbedContainer) return;
 
     const params = new URLSearchParams(window.location.search);
     const clipId = params.get("clipId");
@@ -14,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const domain = window.location.hostname;
 
-    twitchEmbed.src = `https://clips.twitch.tv/embed?clip=clipId&parent=${domain}&autoplay=true&muted=false`;
+    twitchEmbedContainer.src = `https://clips.twitch.tv/embed?clip=${clipId}&parent=${domain}&autoplay=true&muted=false`;
   }
-  twitchEmbedToken();
+  twitchEmbedContainerToken();
 });
