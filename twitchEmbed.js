92b842493705ef1b6212118195691922171890f9
Shiml_der_Gamer47 <161841886+ShimlDerGamer47@users.noreply.github.com>
2025-05-30 21:32:08 +0200

    twitchEmbed.js

--- twitchEmbed.js
+++ twitchEmbed.js
@@ -1,21 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-  function twitchEmbedContainerToken() {
-    const twitchEmbedContainer = document.getElementById(
-      "twitchEmbedContainerId"
-    );
-    if (!twitchEmbedContainer) return;
+  const twitchEmbed = document.getElementById("twitchEmbedId");
+  if (!twitchEmbed) return;
 
-    const params = new URLSearchParams(window.location.search);
-    const clipId = params.get("clipId");
+  const params = new URLSearchParams(window.location.search);
+  const clipId = params.get("clipId");
 
-    if (!clipId) {
-      console.warn("Kein 'clipId' Parameter in der URL gefunden.");
-      return;
-    }
-
-    const domain = window.location.hostname;
-
-    twitchEmbedContainer.src = `https://clips.twitch.tv/embed?clip=${clipId}&parent=${domain}&autoplay=true&muted=false`;
+  if (!clipId) {
+    console.warn("Kein 'clipId' Parameter in der URL gefunden.");
+    return;
   }
-  twitchEmbedContainerToken();
+
+  const domain = window.location.hostname;
+  const embedUrl = `https://clips.twitch.tv/embed?clip=${clipId}&parent=${domain}&autoplay=true&muted=false`;
+
+  twitchEmbed.src = embedUrl;
 });
