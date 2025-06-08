26d30ac62764324bfb3d2398c217b58dd2184812
Shiml_der_Gamer47 <161841886+ShimlDerGamer47@users.noreply.github.com>
2025-06-08 15:50:48 +0200

    twitchEmbed.js

--- twitchEmbed.js
+++ twitchEmbed.js
@@ -5,12 +5,75 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const params = new URLSearchParams(window.location.search);
   const clipId = params.get("clipId");
-  const autoplay = params.get("autoplay") === "true";
-  const muted = params.get("muted") === "true";
 
-  if (!clipId) {
+  if (!clipId || clipId.trim() === "") {
     console.warn("Kein 'clipId' Parameter in der URL gefunden.");
+
+    const divErrorElement = document.createElement("div");
+    Object.assign(divErrorElement.style, {
+      background: "#000000",
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      display: "flex",
+      alignItems: "center",
+      alignContent: "center",
+      justifyItems: "center",
+      justifyContent: "center",
+      border: "2px solid black",
+      borderRadius: "5px",
+      padding: "5px 5px",
+      margin: "10px 10px 10px 10px"
+    });
+    body.appendChild(divErrorElement);
+
+    const pErrorElement = document.createElement("p");
+    Object.assign(pErrorElement.style, {
+      fontFamily: "Arial, Helvetica, sans-serif",
+      background: "#000000",
+      display: "flex",
+      alignItems: "center",
+      alignContent: "center",
+      justifyItems: "center",
+      justifyContent: "center",
+      border: "1px solid transparent",
+      borderRadius: "5px",
+      padding: "5px 5px",
+      margin: "5px 5px 5px 5px",
+      textAlign: "center",
+      fontSize: "25px",
+      color: "#ffffff",
+      textDecoration: "none"
+    });
+    pErrorElement.innerHTML =
+      "Bitte gib eine Clip-ID an!<br>?clipId=1234&autoplay=true&muted=true";
+    divErrorElement.appendChild(pErrorElement);
+
     return;
+  }
+
+  let autoplay = localStorage.getItem("autoplay");
+  let muted = localStorage.getItem("muted");
+
+  if (params.has("autoplay")) {
+    autoplay = params.get("autoplay") === "true";
+    localStorage.setItem("autoplay", autoplay);
+  } else if (autoplay !== null) {
+    autoplay = autoplay === "true";
+  } else {
+    autoplay = false;
+    localStorage.setItem("autoplay", autoplay);
+  }
+
+  if (params.has("muted")) {
+    muted = params.get("muted") === "true";
+    localStorage.setItem("muted", muted);
+  } else if (muted !== null) {
+    muted = muted === "true";
+  } else {
+    muted = true;
+    localStorage.setItem("muted", muted);
   }
 
   const domain = window.location.hostname;
@@ -26,8 +89,4 @@ document.addEventListener("DOMContentLoaded", function () {
   const embedUrl = `https://clips.twitch.tv/embed/?${embedParams.toString()}`;
 
   twitchEmbed.src = embedUrl;
-
-  const scriptElement = document.createElement("script");
-  scriptElement.src = "https://player.twitch.tv/js/embed/v1.js";
-  body.appendChild(scriptElement);
 });
